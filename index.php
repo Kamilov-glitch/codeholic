789afcf020936d74a88ccfda07b5a8412147016e
Fuad <kamilovfuad@gmail.com>
2022-03-23 12:19:03 +0400

    Create header, footer, update.php, add changes to index and view

--- index.php
+++ index.php
@@ -2,20 +2,12 @@
 require 'users.php';
 
 $users = getUsers();
-
+include 'partials/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple PHP CRUD</title>
-</head>
-<body>
-<table class="table">
-    <thead>
+<div class="container">
+    <table class="table">
+        <thead>
         <tr>
             <th>Name</th>
             <th>Username</th>
@@ -24,23 +16,30 @@ $users = getUsers();
             <th>Website</th>
             <th>Actions</th>
         </tr>
-    </thead>
-    <tbody>
+        </thead>
+        <tbody>
         <?php foreach ($users as $user): ?>
-        <tr>
-            <td><?php echo $user['name'] ?></td>
-            <td><?php echo $user['username'] ?></td>
-            <td><?php echo $user['email'] ?></td>
-            <td><?php echo $user['phone'] ?></td>
-            <td><?php echo $user['website'] ?></td>
-            <td>
-                <a href="view.php?id=<?php echo $user['id'] ?>" class="btn-outline-info">View</a>
-                <a href="update.php?id=<?php echo $user['id'] ?>" class="btn-outline-secondary">Update</a>
-                <a href="delete.php?id=<?php echo $user['id'] ?>" class="btn-outline-danger">Delete</a>
-            </td>
-        </tr>
+            <tr>
+                <td><?php echo $user['name'] ?></td>
+                <td><?php echo $user['username'] ?></td>
+                <td><?php echo $user['email'] ?></td>
+                <td><?php echo $user['phone'] ?></td>
+                <td>
+                    <a target="_blank" href="http://<?php echo $user['website'] ?>">
+                        <?php echo $user['website'] ?>
+                    </a>
+                </td>
+                <td>
+                    <a href="view.php?id=<?php echo $user['id'] ?>" class="btn-outline-info">View</a>
+                    <a href="update.php?id=<?php echo $user['id'] ?>" class="btn-outline-secondary">Update</a>
+                    <a href="delete.php?id=<?php echo $user['id'] ?>" class="btn-outline-danger">Delete</a>
+                </td>
+            </tr>
         <?php endforeach; ?>
-    </tbody>
-</table>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
+
+<?php include 'partials/footer.php'; ?>
